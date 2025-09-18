8a0b7e53ec3909730bc31c5cf95abdc6b9ea0141
luk17006 <luk17006@byui.edu>
2025-09-17 18:02:40 -0600

    Course setup commit

---
+++ ponder/week1/meta-css.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+let radius = 3;
+
+// var is old school. Use let instead.
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
